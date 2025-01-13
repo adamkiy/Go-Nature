a63fede7c639511a20343aa3a1e5b2f551d08799
adamkiy <153337416+adamkiy@users.noreply.github.com>
2025-01-13 21:58:01 +0200

    update

--- GoNatureProject-master/GoNature_Client/src/enums/OrderStatus.java
+++
@@ -1,7 +0,0 @@
-package enums;
-
-public enum OrderStatus {
-	
-	pending , processing , canceled , completed ;
-
-}
--- GoNatureProject-master/GoNature_Client/src/gui/AbstractManagerScreen.java
+++
@@ -1,6 +0,0 @@
-package gui;
-
-public abstract class AbstractManagerScreen {
-	
-
-}
